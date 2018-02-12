f6bf5689fd0cbbb2f10cc285ca7c769e87568d2f
samaybar <saybar@gmail.com>
2018-02-12 16:18:53 -0500

    prep to restore

--- runscopeMaintenance.js
+++ runscopeMaintenance.js
@@ -2,16 +2,36 @@
 
 const fs = require('fs');
 const axios = require('axios');
+const moment = require("moment");
 const log = require("./lib/helpers/logger");
 //settings.js file with apikey, buckets to modify location on scheduled environments
 const settings = require('./settings.js');
-const {apikey, buckets, altUrl} = settings;
+let {apikey, buckets, altUrl} = settings;
 const apiUrl = "https://api.runscope.com";
 
 //array to store scheduled tests
 let testSchedules = [];
-//array of environments that are scehedule, and whether they are shared
-let schedudedEnvironments = [];
+
+//restoreFileData
+let restoreData = [];
+
+
+var args = process.argv.slice(2);
+log.debug(args[0]);
+if (!((args[0] === 'pause') || (args[0] === 'resume'))) {
+    throw new Error('FAIL: You must indicate "pause" or "resume"');
+}
+
+
+
+
+   var operation = args[0];
+   if(args[0] === 'resume' && !args[1]) {
+   	throw new Error('FAIL: You must provide a filename to restore schedules')
+   }
+   var restoreFileName = args[1];
+log.debug(args);
+log.debug(args.length);
 const baseUrl = altUrl || apiUrl;
 log.debug(baseUrl);
 
@@ -25,23 +45,24 @@ function getRunscope(endpointUrl) {
 	return axios.get(endpointUrl,);
 }
 
+function deleteRunscopeSchedule(endpointUrl) {
+	return axios.delete(endpointUrl,);
+}
+
+function writeRunscopeSchedule(endpointUrl,postData) {
+	return axios.post(endpointUrl,postData);
+}
+
 (async function run() {
 	try {
 		for (let i = 0; i < buckets.length; i++){
 			let bucket_key = buckets[i];
 			log.info(`Getting bucket ${bucket_key}`)
 
-			//get list of shared environments for the bucket
-			const sharedEndpoint = `${baseUrl}/buckets/${bucket_key}/environments`;
-			const sharedEnvs = await getRunscope(sharedEndpoint);
-			const sharedEnvArray = sharedEnvs.data.data;
-			//create an array of Shared Environments to see if scheduled environments are shared
-			const sharedEnvIds = sharedEnvArray.map(a => a.id);
-			log.debug(sharedEnvIds);
-
 			//get list of tests for this bucket
 			const testList = `${baseUrl}/buckets/${bucket_key}/tests?count=100`;
 			const results = await getRunscope(testList);
+			log.debug(`This is the status code: ${results.status}`);
 			
 			let thisBatch = results.data.data;
 			for (let j = 0; j < thisBatch.length; j++){
@@ -59,77 +80,61 @@ function getRunscope(endpointUrl) {
 						"schedule" : scheduleData
 					}
 
-					for (let k = 0; k < scheduleData.length; k++) {
-
-						let schedEnv = scheduleData[k].environment_id;
-						let isShared = sharedEnvIds.indexOf(schedEnv) > 0;
-						let thisScheduledEnvironment = {
-							"bucket_key": bucket_key,
-							"test_id": test_id,
-							"environment_id": schedEnv,
-							"isShared": isShared,
-							"interval": scheduleData[k].interval,
-							"doesInherit": false
-						};
-
-						//find out if we have this environment already
-						let obj = schedudedEnvironments.find(function (obj) { return obj.environment_id === schedEnv; });
-						log.debug('checking duplicates');
-						if(!obj){
-							log.debug('getting environment details');
-							let envEndpoint;
-							if(isShared){
-								envEndpoint = `${baseUrl}/buckets/${bucket_key}/environments/${schedEnv}`
-							} else {
-								envEndpoint = `${baseUrl}/buckets/${bucket_key}/tests/${test_id}/environments/${schedEnv}`
-							}
-
-							//get details of the environment
-							log.warn(envEndpoint);
-							const environmentDetailsResults = await getRunscope(envEndpoint);
-							const environmentDetailsData = environmentDetailsResults.data.data
-							//for shared environment, get locations & agent
-							if(isShared){
-								thisScheduledEnvironment.regions = environmentDetailsResults.regions;
-								thisScheduledEnvironment.remote_agents = environmentDetailsResults.remote_agents;
-								environmentDetailsResults.regions = [];
-								environmentDetailsResults.remote_agents = [];
-							} else {
-								//first check to see if it inherits from a shared environment
-								if (environmentDetailsData.parent_environment_id) {
-									log.warn(`We have a parent ID ${environmentDetailsData.parent_environment_id}`);
-									const thisParent = environmentDetailsData.parent_environment_id;
-									thisScheduledEnvironment.doesInherit = true;
-									thisScheduledEnvironment.parent_id = thisParent;
-									//check to see if we already have this parent
-									let parentObj = schedudedEnvironments.find(function (parentObj) { return parentObj.environment_id === thisParent; });
-									if(parentObj){
-										log.warn('we already have this parent');
-									} else {
-										log.warn('this parent is new');
-									}
-
-								}
-							}
-							log.debug(environmentDetailsResults.data);
-							
-						} else {
-							//get info from duplicate
-							log.warn(obj);
-						}
-
-						schedudedEnvironments.push(thisScheduledEnvironment);
-					}
-
-
 					//array of test schedules
 					testSchedules.push(thisScheduleData);
 					//log.debug(JSON.stringify(thisScheduleData,undefined,4));
 				}
 			}
 			
-			log.debug(testSchedules);
-			log.debug(schedudedEnvironments);
+			log.debug(JSON.stringify(testSchedules,undefined,4));
+			let jsonWriteData = JSON.stringify(testSchedules,undefined,4);
+			let jsonFileName = `${bucket_key}-${moment().format("YYYYMMDDHmmss")}.json`;
+			writeToFile(jsonWriteData,jsonFileName);
+
+			log.debug("going to write tasks to delete");
+			
+			//this should probably be a function...
+			//deleteListOfSchedules(testSchedules);
+			let taskList = [];
+			//loop through tests in file
+			for (let count = 0; count < testSchedules.length; count++){
+				let thisTestSched = testSchedules[count];
+				let thisBucket = thisTestSched.bucket_key;
+				let thisTest = thisTestSched.test_id;
+				//loop through schedules in test
+				log.debug("loop through schedules");
+				for (let schedCount = 0; schedCount < thisTestSched.schedule.length; schedCount++){
+					let thisRestore = {};
+					let restoreUrl = `${baseUrl}/buckets/${thisBucket}/tests/${thisTest}/schedules`;
+					let deleteUrl = `${restoreUrl}/${thisTestSched.schedule[schedCount].id}`; 
+					log.debug(`URL to delete: ${deleteUrl}`);
+					thisRestore.url = restoreUrl;
+					
+					thisRestore.data = {
+						"environment_id": thisTestSched.schedule[schedCount].environment_id,
+						"interval": thisTestSched.schedule[schedCount].interval,
+						"note": thisTestSched.schedule[schedCount].note
+					}; 
+					taskList.push(deleteUrl);
+					//deleteRunscopeSchedule
+					const deleteResult = await deleteRunscopeSchedule(deleteUrl);
+					thisRestore.deleteStatus = deleteResult.status;
+					const createResults = await writeRunscopeSchedule(thisRestore.url,thisRestore.data)
+					thisRestore.restoreStatus = createResults.status
+					restoreData.push(thisRestore);
+				}
+			}
+
+			log.warn(taskList);
+			jsonWriteData = JSON.stringify(restoreData,undefined,4);
+			jsonFileName = `restore-${bucket_key}-${moment().format("YYYYMMDDHmmss")}.json`;
+			writeToFile(jsonWriteData,jsonFileName);
+
+
+
+
+
+			//log.debug(schedudedEnvironments);
 		}		
 	} catch (e) {
     log.warn(e);
@@ -137,9 +142,14 @@ function getRunscope(endpointUrl) {
 })();
 //const schedUrl = `${baseUrl}/buckets/${bucket_key}/tests/${test_id}/schedules`
 //
-function checkAndAdd(name) {
-  var found = arr.some(function (el) {
-    return el.username === name;
-  });
-  if (!found) { arr.push({ id: id, username: name }); }
+
+
+//writes data to file
+function writeToFile(outputData,fileName){
+  fs.appendFile(fileName, outputData, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("Saved data to: "+ fileName);
+  });  
 }
